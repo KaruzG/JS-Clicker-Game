44fe2f29fe459f45e1222973104f6463b96b4053
Karuz <95084763+KaruzG@users.noreply.github.com>
2022-05-04 15:40:35 +0200

    Add files via upload

--- html/JS/index.js
+++ html/JS/index.js
@@ -1,12 +1,14 @@
-let clickMe = document.getElementById("clickMe");
+document.getElementById('unclickedImg').setAttribute('draggable', false);
 let clickCounter = document.getElementById("clickCounter");
 let clickPower = 1;
 let score = 0;
 let clicksPasivos = 0;
 
 
-if (localStorage.getItem("clicksCache")){
-    score = parseInt(localStorage.getItem("clicksCache"))
+if (localStorage.getItem("score")){
+    score = parseInt(localStorage.getItem("score"));
+    clickPower = parseInt(localStorage.getItem("clickPower"));
+    clicksPasivos = parseInt(localStorage.getItem("clicksPasivos"));
     clickCounter.innerHTML = score;
 } else{
     score = 0
@@ -46,8 +48,8 @@ setInterval (function() {
 }, 1000);
 
 setInterval (function cache() {
-    localStorage
-        .setItem("score", score)
-        .setItem
-
+    localStorage.setItem("score", score);
+    localStorage.setItem("clickPower", clickPower);
+    localStorage.setItem("clicksPasivos", clicksPasivos);
+    console.log("Cache!")
 }, 3000);
